aa9c5ddfa5da7e3c982b89a4af12a095f0f5f887
vc <ktjnetsc@qq.com>
2026-07-07 22:50:38 +0800

    版本号递增至 1.1.30

--- NatTypeTester-Android/app/build.gradle.kts
+++ NatTypeTester-Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nattype.tester"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.1.29"
+        versionCode = 30
+        versionName = "1.1.30"
     }
 
     signingConfigs {
